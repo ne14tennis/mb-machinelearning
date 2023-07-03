ad5dfc56e0681f98cbc26996842f1b8a739e054e
chrismediagithub <chris@mediabrain.io>
2023-07-03 12:57:58 +0000

    retrying 0/1 creation again with subset- dem1

--- dem_sql.sql
+++ dem_sql.sql
@@ -1,3 +1,3 @@
 SELECT DISTINCT segment_id, hh_id
-FROM mediabrain.segment_households
-WHERE broadcast_month = date '2023-05-01'
+FROM mb_monthly_tables.prod_mb_all_hhids_23_05_v1
+WHERE market_id BETWEEN 2204 AND 2450
