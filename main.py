130ffacfc2b120b9d6065363cec2097490f3a173
chrismediagithub <chris@mediabrain.io>
2023-07-31 10:03:23 +0000

    pre=processing+batch 1+ fs+ resampling (batch 2)+model_sel(confusion_matrix heatmap)_ X_train_nw + y_train_nw

--- main.py
+++ main.py
@@ -572,7 +572,7 @@ def run_program(main):
     model = tf.keras.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=(X_train_comb.shape[1],)),
     tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')  # Sigmoid activation fn
+    tf.keras.layers.Dense(1, activation='sigmoid')  
     ])
 
     # Defining the optimizer with a learning rate of 0.01
